ad041986f3a6e98fbbb7774b24eb776cf88fd06d
Agustin Fraile <agustinfraile98@gmail.com>
2024-08-19 20:56:36 -0300

    agregando efecto maquina de escribir

--- src/pages/Intro/Intro.jsx
+++ src/pages/Intro/Intro.jsx
@@ -1,28 +1,37 @@
 import profilePicture from '../../assets/images/image-profiles.png';
 import AOS from 'aos';
+import Typewriter from 'typewriter-effect';
 
 import './Intro.css';
 
 const Intro = () => {
-  AOS.init();
-  return (
-    <section className='section-intro'>
-        <div className='section-intro_image-container'>
-            <img src={profilePicture} alt='Imagen de perfil' className='section-intro_image-container--image' />
-        </div>
-
-        <div className='section-intro_title-container'>
-            <div className='section-intro_title-container--title'>
-                <h1>
-                    AGUSTIN FRAILE
-                </h1>
-                <h3>
-                    &lt; Desarrollador Web /&gt;
-                </h3>
+    AOS.init();
+    return (
+        <section className='section-intro'>
+            <div className='section-intro_image-container'>
+                <img src={profilePicture} alt='Imagen de perfil' className='section-intro_image-container--image' />
             </div>
-        </div>       
-    </section>
-  )
+
+            <div className='section-intro_title-container'>
+                <div className='section-intro_title-container--title'>
+                    <h1>
+                        AGUSTIN FRAILE
+                    </h1>
+                    <h3>
+                        <Typewriter
+                            options={{
+                                strings: ['< Desarrollador web />'],
+                                autoStart: true,
+                                loop: true,
+                                delay: 100,  
+                                deleteSpeed: 50,
+                            }}
+                        />
+                    </h3>
+                </div>
+            </div>
+        </section>
+    )
 }
 
 export default Intro    
