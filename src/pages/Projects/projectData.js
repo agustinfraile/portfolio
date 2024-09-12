6858f4ae6568dce751d776eb5b691dae6beb3b88
Agustin Fraile <agustinfraile98@gmail.com>
2024-09-12 11:11:40 -0300

    agregando gifs

--- src/pages/Projects/projectData.js
+++ src/pages/Projects/projectData.js
@@ -1,30 +1,32 @@
+import webHoneneyGif from '../../assets/images/gifs/web_honey.gif';
+
 export const projectData = [
     {
         title: 'Huellitas',
         description: 'Portal en linea para adopción responsable de mascotas y apoyo a organizaciones que colaboran con el rescate y cuidado de animales domésticos.',
         tech: ['React', 'Node.js', 'PostegreSQL'],
-        imageUrl: 'path_to_image_1.jpg',
+        imageUrl: `${webHoneneyGif}`,
         link: 'https://github.com/agustinfraile/Huellitas'
     },
     {
         title: 'Web Honey',
         description: 'Web dedicada a Honey, mi perrita. La página contiene información sobre su historia y anécdotas divertidas. También incluye fotos para que los visitantes puedan conocerla mejor y disfrutar de su dulce personalidad.',
         tech: ['HTML', 'CSS', 'Bootstrap', 'SaSS'],
-        imageUrl: 'path_to_image_2.jpg',
+        imageUrl: `${webHoneneyGif}`,
         link: 'https://github.com/agustinfraile/webhoney'
     },
     {
         title: 'Web Honey',
         description: 'Web dedicada a Honey, mi perrita. La página contiene información sobre su historia y anécdotas divertidas. También incluye fotos para que los visitantes puedan conocerla mejor y disfrutar de su dulce personalidad.',
         tech: ['HTML', 'CSS', 'Bootstrap', 'SaSS'],
-        imageUrl: 'path_to_image_2.jpg',
+        imageUrl: `${webHoneneyGif}`,
         link: 'https://github.com/agustinfraile/webhoney'
     },
     {
         title: 'Web Honey',
         description: 'Web dedicada a Honey, mi perrita. La página contiene información sobre su historia y anécdotas divertidas. También incluye fotos para que los visitantes puedan conocerla mejor y disfrutar de su dulce personalidad.',
         tech: ['HTML', 'CSS', 'Bootstrap', 'SaSS'],
-        imageUrl: 'path_to_image_2.jpg',
+        imageUrl: `${webHoneneyGif}`,
         link: 'https://github.com/agustinfraile/webhoney'
     },
 ];
