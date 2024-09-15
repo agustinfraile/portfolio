413d9943e33dd71c1017f7e3f6108b8f496d2dce
Agustin Fraile <agustinfraile98@gmail.com>
2024-09-15 00:22:45 -0300

    cambios de importaciones

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
 import Home from './pages/Home/Home';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Error from './pages/Error/Error';
-import Navbar from './components/Navbar/Navbar';
 
 import './App.css';
+import Navbar from './components/NavBar/NavBar';
 
 function App() {
 
   return (
     <BrowserRouter>
 
+
       <Navbar />
+      
       <div className="App">
         <Routes>
           <Route exact path='/' element={<Home />} />
